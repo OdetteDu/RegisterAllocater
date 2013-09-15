a782c98019dfcf33bb95dd8cd91858e6c1255570
OdetteDu <Odette.Du@rice.edu>
2013-09-15 11:18:57 -0500

    Read the file and output to the console

--- main.cpp
+++ main.cpp
@@ -1,8 +1,52 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+
 using namespace std;
 
-int main ()
+void readFile(string path)
 {
-  cout << "Hello World!";
-  return 0;
+	string line;
+	ifstream myfile (path);
+	if (myfile.is_open())
+	{
+		while ( getline (myfile,line) )
+		{
+			cout << line << endl;
+		}
+		myfile.close();
+	}
+
+	else cout << "Unable to open file"; 
 }
+
+int main (int argc, const char* argv[])
+{
+	string mode=argv[1];
+
+	if(mode.compare("t")==0)
+	{
+		cout<<"Use Top-Down Mode\n";
+	}
+	else if(mode.compare("b")==0)
+	{
+		cout<<"Use Buttom-Up Mode\n";
+	}
+	else
+	{
+		cout<<"InvalidArgumentException: the argument entered is not valid, should be either b for buttom up, or t for top down.\n";
+	}
+
+	int numRegisters=stoi(argv[2]);
+	cout<<"Use "<<numRegisters<<" registers.\n";
+
+	string path=argv[3];
+	cout<<path<<"\n";
+	readFile(path);
+
+	while(1)
+	{
+	}
+	return 0;
+}
+
